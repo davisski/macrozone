826af2a80f6973e5853cecc2c953e74e40bc7cf4
Davis Skipsna <skipsna.davis@gmail.com>
2026-04-22 22:58:57 +0300

    #1 - update meal

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,56 @@
+import { colors } from '@/styles/global';
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs, useRouter } from 'expo-router';
+
+export default function TabLayout() {
+    const router = useRouter();
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: colors.background,
+          borderTopColor: colors.surface,
+        },
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textSecondary,
+      }}
+    >
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='home' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='add-meal'
+        options={{
+          title: 'Add Meal',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='add-circle' size={size} color={color} />
+          ),
+        }}
+        listeners={{
+            tabPress: () => {
+              router.push({
+                pathname: '/(tabs)/add-meal',
+                params: { mealId: null }
+              });
+            }
+          }}
+      />
+      <Tabs.Screen
+        name='meals'
+        options={{
+          title: 'All Meals',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='list' size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
